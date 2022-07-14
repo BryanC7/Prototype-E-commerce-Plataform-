c3a1cc4640bfd35a0e358364f6cf018137921652
BryanC7 <bryancabreram3@gmail.com>
2022-07-14 15:54:54 -0400

    Agregando archivos json y actualizando lógica de formularios (comprobar inicio de sesión entre administrador y cliente)

--- validarFormularios.js
+++ validarFormularios.js
@@ -1,3 +1,5 @@
+//! Validación de formulario 
+
 const inputs = document.querySelectorAll('.input-text')
 
 inputs.forEach( input => {
@@ -57,3 +59,38 @@ function mostrarMensajeDeError(tipoDeInput, input) {
     })
     return mensaje
 }
+
+//! Promise
+    
+const email = document.querySelector('#correo')
+const pass = document.querySelector('#contraseña')
+const btnEntrar = document.querySelector('#btnEntrar')
+
+function login() {
+  const promise = new Promise( (resolve, reject) => {
+    const http = new XMLHttpRequest()
+    http.open('GET', 'http://localhost:3000/profile')
+    http.send()
+
+    http.onload = () => {
+      const response = JSON.parse(http.response)
+      if(http.status >= 400) {
+        reject(response)
+      } else {
+        resolve(response)
+      }
+    };
+  });
+  return promise
+  .then((respuesta) => {
+    if(email.value === respuesta[0].email && pass.value === respuesta[0].pass) {
+        window.location.href = 'index-administrador.html'
+    } else if(email.value === respuesta[1].email && pass.value === respuesta[1].pass) {
+        window.location.href = 'index-cliente.html'
+    } else {
+        alert('No te logeaste, inténtalo nuevamente')
+    }})
+}
+
+btnEntrar.addEventListener('click', login)
+ 
